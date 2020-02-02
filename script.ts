3102d62cd4f081d76b3577908bdeefa7d924e152
Mike <mike@Mikes-MacBook-Pro.local>
2020-02-02 18:26:02 +0100

    Initial commit

---
+++ script.ts
@@ -0,0 +1,80 @@
+async function getData(url:string){
+    const response = await fetch(url);
+    return response.json();
+}
+
+async function fetchy() {
+    const users = await getData("http://localhost:3000/users");
+    const company = await getData("http://localhost:3000/companies");
+    for (let i = 0; i < company.length; i++) {
+        let check = company[i].uri;
+        company[i].users = [];
+        company[i].usersLength = [];
+        for (let j = 0; j < users.length; j++) {
+            if (check === users[j].uris.company) {
+                company[i].users.push(users[j]);
+            }
+        }
+        company[i].usersLength = company[i].users.length
+    }
+    company.sort(compareValues('usersLength'));
+
+    interface User{
+        email: string;
+        name: string;
+    }
+
+    interface Customer{
+        name:string;
+        usersLength:number;
+        users:User[];
+    }
+    company.forEach(function (el:Customer, i:number) {
+        const row = document.createElement('tr');
+        let tdContent =
+            ` 
+        <td>
+            ${el.name}
+        </td>
+        
+       
+        <td>
+            ${el.usersLength}
+        </td>`;
+
+        if (el.usersLength>0){
+            let bttn = `<td id="user-${i}"><button class="btn btn-secondary btn-sm" type="button" data-toggle="collapse" data-target="#user${i}" aria-expanded="false" aria-controls="user${i}"> Expand the list of users </button></td>`;
+            row.innerHTML = tdContent + bttn;
+        }else{
+            row.innerHTML = tdContent;
+        }
+
+        document.querySelector('tbody')!.appendChild(row);
+        for(let j=0; j<el.usersLength; j++) {
+            document.getElementById(`user-${i}`)!.innerHTML += `
+            <div class="collapse" id="user${i}"> 
+                <div class="card card-body"> Name: ${el.users[j].name} <br></br> E-mail: ${el.users[j].email}</div>
+            </div>`
+        }
+    });
+}
+function compareValues(key: string) {
+    return function innerSort(a:any, b:any) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            return 0;
+        }
+        const varA = a[key];
+        const varB = b[key];
+        let comparison = 0;
+
+        if (varA > varB) {
+            comparison = 1;
+
+        }else if (varA < varB) {
+            comparison = -1;
+
+        }return (comparison);
+    };
+}
+
+fetchy();
